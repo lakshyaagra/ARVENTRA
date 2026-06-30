f22622b56ec50c8e89c87ab7c5a3f37409ec8776
Lakshya Agarwal <lakshyaagrawal72932@gmail.com>
2026-07-01 01:51:20 +0530

    set up first Express Server

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
-console.log("Welcome to Financial OS Backend 🚀");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Financial OS Backend 🚀");
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on Port 3000");
+});
