1a7d61980bf6665cb925f4d866a0ad93c58a3c4c
Lakshya Agarwal <lakshyaagrawal72932@gmail.com>
2026-07-09 00:20:18 +0530

    User Controller created

---
+++ backend/controllers/userController.js
@@ -0,0 +1,65 @@
+const bcrypt=require('bcrypt')
+const User=require('../models/User');
+const jwt=require('jsonwebtoken');
+
+const registerUser=async (req,res)=>{
+    try{
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        req.body.password = hashedPassword;
+        const user = await User.create(req.body);
+        user.password = undefined; //password ko response me nahi bhejna chahte , isliye undefined kar diya
+        res.status(201).json({
+            message: "User Registered",
+            success: true,
+            user: user
+        })
+    }
+    catch(err){
+        res.status(500).json({
+            message: err.message,
+            success: false
+        })
+    }
+}
+// ab tk register krne se databse mai mera information phch ya keh skte hai store ho chuka hai
+const loginUser=async (req,res)=>{
+    try{
+        const user=await User.findOne({
+            email:req.body.email
+        })
+        if(!user){
+            return res.status(401).json({
+                message: "Invalid Email or Password",
+                success: false
+            })
+        }
+        const isPasswordValid=await bcrypt.compare(
+            req.body.password,
+            user.password
+        )
+        if(!isPasswordValid){
+            return res.status(401).json({
+                message: "Invalid Email or Password",
+                success: false
+            })
+        }
+        //JWT WILL BE GENERATED AND SENT TO THE USER AS TOKEN AFTER SUCCESSFULLY BEING LOGIN
+        const token=jwt.sign(
+            {id:user._id},
+            process.env.JWT_SECRET,
+            { expiresIn: "7d"}
+        )
+        res.status(200).json({
+            message: "User Logged In",
+            success: true,
+            token: token
+        })
+    }
+    catch(err){
+        res.status(500).json({
+            message: err.message,
+            success: false
+        })
+    }
+}
+module.exports={registerUser, loginUser}
