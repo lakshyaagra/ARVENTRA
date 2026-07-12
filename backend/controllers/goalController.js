841d88a35c26aa8c916cf45c354e0e0ff0aed327
Lakshya Agarwal <lakshyaagrawal72932@gmail.com>
2026-07-13 02:14:01 +0530

    Added Sorting, Filtering & Pagination in API GET

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -20,17 +20,75 @@ const createGoal = async (req, res) => {
 }
 const getGoals = async (req, res) => {
     try{
+        //sort apis handle
+        const sortField = req.query.sort || 'createdAt';
+        const allowedSortFields = ['createdAt', 'targetAmount', 'goalName','status'];
+        const order=req.query.order || 'desc';
+        if(!allowedSortFields.includes(sortField) || (order !== 'asc' && order !== 'desc')){
+            return res.status(400).json({
+                message: "Invalid sort field or order.",
+                success: false
+            });
+        }
+        const sortOrder = order === 'asc'?1:-1;
+        const sortObject={
+            [sortField]: sortOrder
+        }
+        //filter api handle
+        const allowedStatus = ['active', 'completed'];
+        if(req.query.status && !allowedStatus.includes(req.query.status)){
+            return res.status(400).json({
+                message: "Invalid status value (only active and completed allowed).",
+                success: false
+            });
+        }
+        const filter={
+            user:req.user.id,
+        }
+        if(req.query.status){
+            filter.status=req.query.status
+        }
+
+        //searching
+        const search=req.query.search?.trim()
+        if(search){
+            filter.goalName={
+                $regex:search,
+                $options:"i"
+            }
+        }
+
+        //pagination
+        const page=Number(req.query.page) || 1;
+        const limit=Number(req.query.limit) || 15;
+        const skip=limit*(page-1);
+
+        const totalGoals=await Goal.countDocuments(filter);
+        const totalPages=Math.ceil(totalGoals/limit);
+        const hasNextPage=page<totalPages;
+        const hasPreviousPage=page>1;
+        //api response limiting
+        // const fields=req.query.fields;
+        // let selectedFields;
+        // if(fields){
+        //     selectedFields=fields.replaceAll(","," ") + "user";
+        // }                     
         // const goals=await Goal.find();   //Goal is a Model which is used to fetch data from MongoDB database
-        const goals=await Goal.find(
-            { user: req.user.id }
-        );
+        const goals=await Goal.find(filter)
+        .populate('user',"name email").sort(sortObject).skip(skip).limit(limit);
+
         res.status(200).json({
             message:" Goals Retrieved ",
             success:true,
+            currPage:page,
+            totalGoals,
+            totalPages,
+            hasNextPage,
+            hasPreviousPage,
             goals:goals,
         })
     }    
-    catch(err){
+    catch(err){  //Database Error
         res.status(500).json({
             message: err.message,
             success: false
