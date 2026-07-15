013029c298355c2cbdd92ed3645ee50cda8e9405
Lakshya Agarwal <lakshyaagrawal72932@gmail.com>
2026-07-15 17:15:56 +0530

    Modified goal route for image uploads

--- backend/routes/goals.js
+++ backend/routes/goals.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const validateGoal = require("../middleware/validateGoal");
+const validateUpdateGoal= require('../middleware/validateUpdateGoal')
 const { createGoal,getGoals,getGoalById,updateGoalById, deleteGoalById } = require("../controllers/goalController");
+const authMiddleware = require('../middleware/authMiddleware');
+const upload=require('../config/multer')
 
-router.post('/',validateGoal, createGoal);
-router.get('/', getGoals);
-router.get('/:id', getGoalById);
-router.put('/:id', validateGoal, updateGoalById);
-router.delete('/:id', deleteGoalById);
+router.post('/',authMiddleware,
+    upload.single('goalImage') 
+    ,validateUpdateGoal, createGoal
+);
+
+router.get('/',authMiddleware,getGoals); 
+router.get('/:id',authMiddleware, getGoalById);
+router.put('/:id', authMiddleware,upload.single("goalImage"),validateUpdateGoal, updateGoalById);
+router.delete('/:id',authMiddleware, deleteGoalById);
 
 module.exports = router;
