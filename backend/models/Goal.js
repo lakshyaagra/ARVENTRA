4bca8adef7a056a4c7fe61b6915d74a85c855470
Lakshya Agarwal <lakshyaagrawal72932@gmail.com>
2026-07-15 17:14:42 +0530

    Modified and added all remaining important fields for goal schema

--- backend/models/Goal.js
+++ backend/models/Goal.js
@@ -11,10 +11,44 @@ const goalSchema = new mongoose.Schema({
         required: true,
         min:1
     },
+    currentAmount: {
+        type: Number,
+        default: 0,
+        min: 0
+    },
+    priority:{
+        type:String,
+        enum:["low","medium","high"],
+        default:"medium"
+    },
+    category: {
+        type: String,
+        enum: ["education","travel","electronics","vehicle","home","investment","emergency","personal","other"],
+        default: "other"
+    },
+    deadline:{
+        type: Date,
+        validate: {
+            validator: function(value){
+                return !value || value>new Date();
+            },
+            message: "Deadline must be a future date."
+        }
+    },
+    description:{
+        type:String,
+        trim:true
+    },
     status:{
         type: String,
         enum: ['active', 'completed'],
         default: 'active'
+    },
+    image:{
+        type:String,
+    },
+    publicId:{
+        type:String,
     },
     user:{
         type: mongoose.Schema.Types.ObjectId,
