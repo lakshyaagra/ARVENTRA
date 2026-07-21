1bbdfa5fe314b3e629cdf7511e1c10650f1536cd
Lakshya Agarwal <lakshyaagrawal72932@gmail.com>
2026-07-22 02:41:34 +0530

    Validation Middlewares Added

---
+++ backend/middleware/validateIncome.js
@@ -0,0 +1,55 @@
+const validateIncome=(req,res,next)=>{
+    const incomeSource=req.body.incomeSource?.trim();
+    const amount=Number(req.body.amount)
+    const allowedPaymentMethods=["cash","upi","credit-card","debit-card","bank-transfer","wallet","other"];
+    const allowedCategories=["salary","business","freelancing","investment","rental","interest","gift","bonus","refund","other"];
+
+    if(!incomeSource){
+        return res.status(400).json({
+            success: false,
+            message: 'Income Source is required' 
+        })
+    }
+    if(Number.isNaN(amount) || amount<=0){
+        return res.status(400).json({
+            success: false,
+            message: 'Amount must be Positive no.' 
+        })
+    }
+    if(req.body.paymentMethod!==undefined && !allowedPaymentMethods.includes(req.body.paymentMethod)){
+        return res.status(400).json({
+            success: false,
+            message: "Invalid Payment Method type."
+        });
+    }
+    if(req.body.category!==undefined && !allowedCategories.includes(req.body.category)){
+        return res.status(400).json({
+            success: false,
+            message: "Invalid Category."
+        });
+    }
+    if(req.body.receivedDate!==undefined){
+        const receivedDate = new Date(req.body.receivedDate);
+        if (Number.isNaN(receivedDate.getTime())) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid Recieve date."
+            });
+        }
+        if (receivedDate > new Date()) {
+            return res.status(400).json({
+                success: false,
+                message: "Received date cannot be in the future."
+            });
+        }
+    }
+    if(req.body.notes!==undefined && req.body.notes.trim()===""){
+        return res.status(400).json({
+            success:false,
+            message: "Notes can't be empty."
+        })
+    }
+
+    next();
+}
+module.exports=validateIncome;
---
+++ backend/middleware/validateUpdateIncome.js
@@ -0,0 +1,62 @@
+const validateUpdateIncome=(req,res,next)=>{
+    const { incomeSource,amount,category,paymentMethod,receivedDate,notes } = req.body;
+    const allowedCategories=["salary","business","freelancing","investment","rental","interest","gift","bonus","refund","other"];
+    const allowedPaymentMethods=["cash","upi","credit-card","debit-card","bank-transfer","wallet","other"];
+
+
+    if(incomeSource!==undefined){
+        if (incomeSource.trim() === "") {
+            return res.status(400).json({
+                success: false,
+                message: "Income Source cannot be empty."
+            });
+        }
+    }
+    if(amount!==undefined){
+        const parsedAmount=Number(amount);
+        if(Number.isNaN(parsedAmount) || parsedAmount<=0){
+            return res.status(400).json({
+                success: false,
+                message: "Amount must be a positive number"
+            });
+        }
+    }
+    if(category!==undefined && !allowedCategories.includes(category)){
+        return res.status(400).json({
+            success: false,
+            message: "Invalid Category"
+        })
+    }
+    if(paymentMethod!==undefined && !allowedPaymentMethods.includes(paymentMethod)){
+        return res.status(400).json({
+            success: false,
+            message: "Invalid Category"
+        })
+    }
+    if(notes!==undefined){
+        if(typeof notes!=="string" || notes.trim()===""){
+            return res.status(400).json({
+                success:false,
+                message: "Notes can't be empty."
+            })
+        }
+    }
+    if(receivedDate!==undefined){
+        const parsedReceivedDate=new Date(receivedDate);
+        if(Number.isNaN(parsedReceivedDate.getTime())){
+            return res.status(400).json({
+                success:false,
+                message:"Invalid Recieve Date."
+            })
+        }
+        if(parsedReceivedDate>new Date()){
+            return res.status(400).json({
+                success: false,
+                message: "Recieve date cannot be in the future."
+            });
+        }
+    }
+
+    next();
+}
+module.exports=validateUpdateIncome;
