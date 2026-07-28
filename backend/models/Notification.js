9d4effe859657c4d58d3f19d1bb3a41e54836536
Lakshya Agarwal <lakshyaagrawal72932@gmail.com>
2026-07-29 01:12:59 +0530

    Notification Schema added

---
+++ backend/models/Notification.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const notificationSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    message: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    type: {
+        type: String,
+        enum: ["goal","loan","income","expense","report","credit-health","community","system","ai"],
+        required: true
+    },
+    isRead: {
+        type: Boolean,
+        default: false
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model("Notification", notificationSchema);
