d81d7c134792850e0882dc6a8510d1abf8d21dd8
Lakshya Agarwal <lakshyaagrawal72932@gmail.com>
2026-08-05 02:49:20 +0530

    Configured Socket.io

--- backend/services/notificationService.js
+++ backend/services/notificationService.js
@@ -1,6 +1,9 @@
 const Notification = require("../models/Notification");
+const { emitNotification } = require("../socket/socketEmitter");
 
 const createNotification = async ({user,title,message,type}) => {
-    return await Notification.create({ user,title,message,type });
+    const notification = await Notification.create({user,title,message,type});
+    emitNotification(user, notification);
+    return notification;
 };
 module.exports = { createNotification };
