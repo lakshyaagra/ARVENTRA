d210d3f589485acbd07636603d2f026789265db9
Lakshya Agarwal <lakshyaagrawal72932@gmail.com>
2026-08-18 01:31:14 +0530

    Feat: Calculators Completed

--- backend/routes/calculators.js
+++ backend/routes/calculators.js
@@ -1,24 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const authMiddleware = require("../middleware/authMiddleware");
 
 const { simpleInterestCalculator,compoundInterestCalculator,roiCalculator
     ,fdCalculator,rdCalculator,sipCalculator,lumpsumCalculator,
     emiCalculator,incomeTaxCalculator,retirementCalculator
  } = require("../controllers/calculatorsController");
 
-router.get("/simple-interest", authMiddleware, simpleInterestCalculator);
-router.get("/compound-interest", authMiddleware, compoundInterestCalculator);
-router.get("/return-on-investment", authMiddleware, roiCalculator);
-router.get("/fd", authMiddleware, fdCalculator);
-router.get("/rd", authMiddleware, rdCalculator);
-router.get("/sip", authMiddleware, sipCalculator);
-router.get("/lumpsum", authMiddleware, lumpsumCalculator);
-router.get("/personal-loan-emi", authMiddleware, emiCalculator);
-router.get("/home-loan-emi", authMiddleware, emiCalculator);
-router.get("/car-loan-emi", authMiddleware, emiCalculator);
-router.get("/education-loan-emi", authMiddleware, emiCalculator);
-router.get("/income-tax", authMiddleware, incomeTaxCalculator);
-router.get("/retirement", authMiddleware, retirementCalculator);
+router.get("/simple-interest",simpleInterestCalculator);
+router.get("/compound-interest",compoundInterestCalculator);
+router.get("/return-on-investment",roiCalculator);
+router.get("/fd",fdCalculator);
+router.get("/rd",rdCalculator);
+router.get("/sip",sipCalculator);
+router.get("/lumpsum",lumpsumCalculator);
+router.get("/personal-loan-emi",emiCalculator);
+router.get("/home-loan-emi",emiCalculator);
+router.get("/car-loan-emi",emiCalculator);
+router.get("/education-loan-emi",emiCalculator);
+router.get("/income-tax",incomeTaxCalculator);
+router.get("/retirement",retirementCalculator);
 
 module.exports = router;
