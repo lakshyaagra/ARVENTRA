8f8e8803820cf71c3bbab54cf103df0fe8c7f807
Lakshya Agarwal <lakshyaagrawal72932@gmail.com>
2026-08-29 02:51:51 +0530

    Spelling Fixed

--- backend/routes/dashboard.js
+++ backend/routes/dashboard.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middleware/authMiddleware");
-const {getDashboard} =require("../controllers/dashboardController");
+const {getDashboard} =require("../controllers/dashBoardController");
 
 router.get("/",authMiddleware,getDashboard);
 module.exports = router;
